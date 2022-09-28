5a26bfd9395f9551911754b9d87173c86da4a922
mircoho <39863436+ps4gentoo@users.noreply.github.com>
2022-09-28 14:37:52 +0200

    offsets 9.00

--- source/main.c
+++ source/main.c
@@ -40,17 +40,17 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	//Reading kernel_base...
 	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-KERN_XFAST_SYSCALL];
 	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
-	void** got_prison0 =   (void**)&kernel_ptr[0x113E518];
-	void** got_rootvnode = (void**)&kernel_ptr[0x2300320];
+	void** got_prison0 =   (void**)&kernel_ptr[0x111F870];
+	void** got_rootvnode = (void**)&kernel_ptr[0x21EFF20];
 
 	//Resolve kernel functions...
-	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x3C16B0);
-	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x123280);
-	int (*set_nclk_mem_spd)(int val) = (void *)(kernel_base + 0x19A390);
-	int (*set_pstate)(int val) = (void *)(kernel_base + 0x4FC2E0);
-	int (*set_gpu_freq)(int cu, unsigned int freq) = (void *)(kernel_base + 0x4E0530);
-	int (*update_vddnp)(unsigned int cu) = (void *)(kernel_base + 0x4E0B10);
-	int (*set_cu_power_gate)(unsigned int cu) = (void *)(kernel_base + 0x4E0F20);
+	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x2715B0);
+	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x0B7A30);
+	int (*set_nclk_mem_spd)(int val) = (void *)(kernel_base + 0x13FAD0);
+	int (*set_pstate)(int val) = (void *)(kernel_base + 0x4D6FC0);
+	int (*set_gpu_freq)(int cu, unsigned int freq) = (void *)(kernel_base + 0x4DDDC0);
+	int (*update_vddnp)(unsigned int cu) = (void *)(kernel_base + 0x4DE360);
+	int (*set_cu_power_gate)(unsigned int cu) = (void *)(kernel_base + 0x4DE770);
 	
 	cred->cr_uid = 0;
 	cred->cr_ruid = 0;
@@ -79,18 +79,18 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	uint64_t cr0 = readCr0();
 	writeCr0(cr0 & ~X86_CR0_WP);
 	
-	kernel_ptr[0x20734E] = 3; //5.05 pstate when shutdown
+	kernel_ptr[0x29A96F] = 3; //5.05 pstate when shutdown
 
 	//Kexec init
-	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xC00468); // I know it's for 4.55 but I think it will works
+	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xBE3080); // I know it's for 4.55 but I think it will works
 	memcpy(DT_HASH_SEGMENT, kexec_data, kexec_size);
 
 	void (*kexec_init)(void *, void *) = DT_HASH_SEGMENT;
 
-	kexec_init((void *)(kernel_base+0x123280), NULL);
+	kexec_init((void *)(kernel_base+0x0B7A30), NULL);
 
 	// Say hello and put the kernel base in userland to we can use later
-	printfkernel("PS4 Linux Loader for 6.72 by valentinbreiz\n");
+	printfkernel("PS4 Linux Loader for 9.00 by valentinbreiz\n");
 
 	printfkernel("kernel base is:0x%016llx\n", kernel_base);
 
